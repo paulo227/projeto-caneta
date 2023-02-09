00636f3af0a761dcb8a5ab5831093f9cc6e5fa44
paulo felipe <paullfelipe268@gmail.com>
2023-02-09 16:32:22 -0300

    commit da visibilidade

--- curso_poo/Caneta.java
+++ curso_poo/Caneta.java
@@ -1,30 +1,30 @@
 package curso_poo;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    double ponta;
-    int carga;
-    boolean tampada;
+   public String modelo;
+    public String cor;
+    private double ponta = 0.5;
+    private int carga = 2;
+    public boolean tampada;
 
-    void status(){
+    public void status(){
         System.out.println("modelo: " + this.modelo);
         System.out.println("Cor: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
         System.out.println("Tampada? " + this.tampada);
     }
-    void rabiscar(){
+   public void rabiscar(){
         if (tampada == true){
             System.out.println("ERRO, caneta tampada");
         }else {
             System.out.println("RABISCO, caneta destampada");
         }
     }
-    void tampar(){
+    public void tampar(){
         tampada = true;
     }
-    void  destampar(){
+   public void  destampar(){
         tampada = false;
     }
 }
