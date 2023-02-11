572bcbd9909706c3f531901b3a477bf11b34819b
paulo felipe <paullfelipe268@gmail.com>
2023-02-11 14:57:49 -0300

    commit dos métodos especiais
    Comitando os métodos especiais getter, setter e construtor

--- curso_poo/Caneta.java
+++ curso_poo/Caneta.java
@@ -7,6 +7,52 @@ public class Caneta {
     private int carga = 2;
     public boolean tampada;
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(double ponta) {
+        this.ponta = ponta;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public Caneta(String modelo, String cor, boolean tampada) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.tampada = tampada;
+    }
+
     public void status(){
         System.out.println("modelo: " + this.modelo);
         System.out.println("Cor: " + this.cor);
--- curso_poo/ObjetoCaneta.java
+++ curso_poo/ObjetoCaneta.java
@@ -2,18 +2,8 @@ package curso_poo;
 
 public class ObjetoCaneta {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.cor = "Azul";
-        c1.modelo = "Bic";
-        c1.tampada = true;
+        Caneta c1 = new Caneta("Bic", "Azul", false);
         c1.status();
         c1.rabiscar();
-        System.out.println("----------------");
-        Caneta c2 = new Caneta();
-        c2.cor = "Preto";
-        c2.modelo = "Hostnet";
-        c2.destampar();
-        c2.status();
-        c2.rabiscar();
     }
 }
